b283853dddb5df7425a1e6e2762cd3532b997f7a
Ojaswin Kaushik <ojaswin.kaushik@oneimpression.io>
2025-05-17 10:21:19 +0530

    minor

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { IconSymbol } from "@/components/IconSymbol";
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="cart" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,103 @@
+import Loader from "@/components/Loader";
+import PriceTag from "@/components/PriceTag";
+import { baseApi, windowWidth } from "@/constants";
+import useFetch from "@/hooks/useFetch";
+import { useProductStore } from "@/hooks/useProductStore";
+import { Image } from "expo-image";
+import { useRouter } from "expo-router";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Rating } from "react-native-ratings";
+
+const router = useRouter();
+
+export default function Index() {
+  const { setAllProducts } = useProductStore();
+  const { loading, data } = useFetch(`${baseApi}products`, (data) =>
+    setAllProducts(data)
+  );
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(_, index) => index.toString()}
+      numColumns={2}
+      style={{ flex: 1 }}
+      contentContainerStyle={styles.container}
+      showsVerticalScrollIndicator={false}
+      ListEmptyComponent={loading ? <Loader /> : <Text>No data found</Text>}
+      renderItem={({ item }) => (
+        <View
+          style={styles.item}
+          onTouchEnd={() => {
+            router.push({
+              pathname: "/products/[id]" as any,
+              params: { id: item.id.toString() },
+            });
+          }}
+        >
+          <Image
+            source={item?.image}
+            contentFit="contain"
+            style={styles.image}
+          />
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 10,
+              maxHeight: 40,
+            }}
+          >
+            <Rating
+              type="star"
+              ratingCount={5}
+              imageSize={15}
+              startingValue={item?.rating?.rate}
+              readonly
+            />
+            <Text style={{ fontSize: 12, marginLeft: 4 }}>
+              ({item?.rating?.rate})
+            </Text>
+          </View>
+          <Text style={styles.text} numberOfLines={2} ellipsizeMode="tail">
+            {item?.title}
+          </Text>
+          <PriceTag price={item?.price} />
+        </View>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffe6e6",
+    opacity: 0.8,
+    flexGrow: 1,
+  },
+  item: {
+    flex: 1,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "#ccccb3",
+    overflow: "hidden",
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    position: "relative",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: "bold",
+    padding: 10,
+  },
+  image: {
+    width: windowWidth / 2 - 20,
+    height: 150,
+    backgroundColor: "white",
+  },
+});
